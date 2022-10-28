c5adf453d117d684b5c5679eeda7e7c37c47af8f
Enei <eneipereira@outlook.com.br>
2022-10-28 15:10:51 -0300

    test: Cria teste que verifica se, ao criar um novo produto, ele é criado com todos os atributos corretamente preenchidos.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+PARAMS = [
+    1,
+    "Teste",
+    "Empresa Fictícia",
+    "2022-10-28",
+    "2024-12-25",
+    "123456789",
+    "Guardar direitinho"
+    ]
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(*PARAMS)
+
+    assert new_product.id == PARAMS[0]
+    assert new_product.nome_do_produto == PARAMS[1]
+    assert new_product.nome_da_empresa == PARAMS[2]
+    assert new_product.data_de_fabricacao == PARAMS[3]
+    assert new_product.data_de_validade == PARAMS[4]
+    assert new_product.numero_de_serie == PARAMS[5]
+    assert new_product.instrucoes_de_armazenamento == PARAMS[6]
