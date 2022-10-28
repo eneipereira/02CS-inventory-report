9e237e7ac8191ce8ab46650600ef332d418da8e8
Enei <eneipereira@outlook.com.br>
2022-10-28 16:29:57 -0300

    test: Cria teste que verifica se, ao criar um novo produto, é gerado corretamente um relatório sobre ele.

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+PARAMS = [
+    1,
+    "Teste",
+    "Empresa Fictícia",
+    "2022-10-28",
+    "2024-12-25",
+    "123456789",
+    "corretamente"
+    ]
+
+REPORT_PHRASE = (
+    "O produto Teste fabricado em 2022-10-28 por Empresa Fictícia "
+    "com validade até 2024-12-25 precisa ser armazenado corretamente."
+    )
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(*PARAMS)
+
+    assert str(new_product) == REPORT_PHRASE
