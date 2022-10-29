8cde1621cd82956808a251394928172ac1e09b79
Enei <eneipereira@outlook.com.br>
2022-10-29 16:33:53 -0300

    feat: Implementa a classe CompleteReport herdando da SimpleReport, que gera uma versão completa do relatório com as informações corretas.

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,32 @@
+from .simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def __quantity_by_company(data):
+        companies = dict()
+
+        for dado in data:
+            company = dado["nome_da_empresa"]
+
+            if company not in companies:
+                companies[company] = 1
+            else:
+                companies[company] += 1
+
+        return companies
+
+    @staticmethod
+    def generate(data: "list[dict]"):
+        simple_report = SimpleReport.generate(data)
+
+        companies_dict = CompleteReport.__quantity_by_company(data)
+
+        remaining_report = "Produtos estocados por empresa:\n"
+
+        for (company, quantity) in companies_dict.items():
+            remaining_report += f"- {company}: {quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"{remaining_report}"
+        )
