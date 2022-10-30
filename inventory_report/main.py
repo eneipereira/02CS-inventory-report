aabb87502016b5035e14b810086d42558b251fa5
Enei <eneipereira@outlook.com.br>
2022-10-30 00:03:11 -0300

    feat: Implementa a função main, para que, ao receber pela linha de comando o caminho de um arquivo e o tipo do relatório, seja devolvido o relatório correto.

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+from pathlib import Path
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    args = sys.argv
+
+    if len(args) < 3:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+
+    _, path, file_type = args
+
+    file_ext = Path(path).suffix
+
+    if file_ext == '.csv':
+        importer = CsvImporter
+    elif file_ext == '.json':
+        importer = JsonImporter
+    else:
+        importer = XmlImporter
+
+    inventory = InventoryRefactor(importer)
+    report = inventory.import_data(path, file_type)
+
+    return print(report, end="")
